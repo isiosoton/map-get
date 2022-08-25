40ce31c1ec0b80ad291d6c13d322f9f599b656df
Kohei-Nakama44 <knakama11796516@gmail.com>
2022-08-25 16:58:40 +0900

    完成まで後少し

--- app_develop/addres_sp.py
+++ app_develop/addres_sp.py
@@ -1,14 +1,11 @@
-# 元の文字列
-text = "！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿｀>？＠ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～"
+# 元の文字列("と\の記号は「\"」「\\」としてエスケープしています。
+text = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
 
 # 変換
-x = text.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
+x = text.translate(str.maketrans({chr(0x0021 + i): chr(0xFF01 + i) for i in range(94)}))
 
 print(x)
+
 # 結果
-# '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`>?@abcdefghijklmnopqrstuvwxyz{|}~'
-
-
-kisoku_a = [["一","1"],["二","2"],["三","3"],["四","4"],["五","5"],["六","6"],["七","7"],["八","8"],["九","9"]]
-for i in kisoku_a:
-    jusyo = jusyo.replace(i[0],i[1])
+# '！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～'
